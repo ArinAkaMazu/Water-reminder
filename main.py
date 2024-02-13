89df87fb77f528823f64378187ecc3ba19a831c7
ArinAkaMazu <arinkaushik60@gmail.com>
2024-02-13 17:23:12 +0530

    set reminder time

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time 
 from plyer import notification 
 if __name__ == "__main__":
-    notification.notify(
-        title= "Drink Water",
-        message="Make sure to stay hydrated and take a sip",
-        app_icon="E:\\Python\\Water reminder\\icon.ico",
-        timeout=10
-    )
+    while True:
+        notification.notify(
+            title= "Drink Water!!",
+            message="Make sure to stay hydrated and take a sip",
+            app_icon="E:\\Python\\Water reminder\\icon.ico",
+            timeout=10
+        )
+        time.sleep(60*60)
